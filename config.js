cd741594fac34112766aeb220606a11fd6b77fdf
davidepastore <pasdavide@gmail.com>
2015-02-05 17:24:26 +0100

    Add all files

---
+++ config.js
@@ -0,0 +1,17 @@
+module.exports = {
+	mongodb: {
+		server: 'localhost',
+		port: 27017,
+		
+		//Database name
+		database: 'comuni',
+		
+		//Collection name
+		collection: 'comuni',
+		auth: false,
+		
+		//Only used if auth is true
+		username: 'admin',
+		password: 'pass'
+  }
+};
