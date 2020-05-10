6f1a41f355cff6431635f6fc6b93932e619bb838
Davide Pastore <pasdavide@gmail.com>
2020-05-10 17:32:52 +0200

    Add more specific tests

--- test/test.js
+++ test/test.js
@@ -1,68 +1,62 @@
-const async = require('async');
 const assert = require('assert');
-const exec = require('child_process').exec;
 var MongoClient = require('mongodb').MongoClient;
-const { spawn, spawnSync } = require( 'child_process' );
-var config = require('../config');
-var utils = require('../utils');
-const TIMEOUT_TIME = 1000 * 60 * 60 * 2; // 2 hours timeout
+
+function findAllCodPostaliBetweenTwo(start, end) {
+    var output = [];
+    for(i = 118; i <= 199; i++) {
+        output.push("00" + i);
+    }
+
+    return output;
+}
 
 describe('Compendio Vicorum', function() {
-  describe('execution', function() {
-    child = spawn( 'node', [ 'app.js' ] );
-
-    it('should connect to the MongoDB server', function(done) {
-        this.timeout(TIMEOUT_TIME); //timeout with an error if done() isn't called within the given time
-        /*
-        async.series([
-            exec('cd ..'),
-            exec('node app.js')
-        ]);
-        */
-        //child = spawn( 'node', [ 'app.js' ] );
-
-        /*
-        child.stdout.on('data', (data) => {
-            console.log(`child stdout:\n${data}`);
-        });
-        
-        child.stderr.on('data', (data) => {
-            console.error(`child stderr:\n${data}`);
-        });
-        */
-
-        child.on('exit', function (code, signal) {
-            console.debug('child process exited with ' + `code ${code} and signal ${signal}`);
-            assert.equal(code, 0);
-            done();
-        });
-    });
-
     it('should retrieve provincia information', function(done) {
-        this.timeout(TIMEOUT_TIME); //timeout with an error if done() isn't called within the given time
-
-        child.on('exit', function (code, signal) {
-            console.log('onexit test test');
-            MongoClient.connect(url, function(err, db) {
-                if(err){
-                    console.log("Error connecting to server");
-                    done();
-                }
-                
-                var collection = db.collection(mongodb.collection);
-
+        MongoClient.connect(url, function(err, db) {
+            if(err){
+                console.log("Error connecting to server");
                 done();
-                
-                //Read the provincia information
-                collection.find({}).toArray(function(err, docs) {
-                    assert.equal(err, null);
-                    docs.each(function(doc){
-                        assert.isDefined(doc.provincia);
-                    });
-                    callback(docs);
+            }
+            
+            var collection = db.collection(mongodb.collection);
+            
+            //Read the provincia information
+            collection.find({}).toArray(function(err, docs) {
+                assert.equal(err, null);
+                docs.each(function(doc){
+                    assert.isDefined(doc.provincia);
                 });
+                done();
+            });
+
+            // TODO Add specific it here
+            // Add check on the "Cod. postale" fields
+            collection.find({"nome": "Abbasanta"}).toArray(function(err, docs) {
+                assert.equal(err, null);
+                docs.each(function(doc){
+                    assert.isDefined(doc.provincia);
+                    assert.equal(doc.codicePostale, ["09071"]);
+                });
+                done();
+            });
+
+            collection.find({"nome": "Palestrina"}).toArray(function(err, docs) {
+                assert.equal(err, null);
+                docs.each(function(doc){
+                    assert.isDefined(doc.provincia);
+                    assert.equal(doc.codicePostale, ["00036"]);
+                });
+                done();
+            });
+
+            collection.find({"nome": "Roma", "tipo": "comune"}).toArray(function(err, docs) {
+                assert.equal(err, null);
+                docs.each(function(doc){
+                    assert.isDefined(doc.provincia);
+                    assert.equal(doc.codicePostale, findAllCodPostaliBetweenTwo("118", "199"));
+                });
+                done();
             });
         });
     });
-  });
 });
