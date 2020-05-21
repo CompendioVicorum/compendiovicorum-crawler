f2df791f1132e0983b970835b71a4fedba77398f
Davide Pastore <d.pastore@logispin.com>
2020-05-21 12:10:29 +0200

    Improve the way the tests are grouped

--- test/utils.js
+++ test/utils.js
@@ -31,91 +31,93 @@ describe('Utils', function () {
     done()
   })
 
-  it('buildSindaco should build the correct sindaco object in the case of one mandate', function (done) {
-    const tdText = 'Giuseppe Sala (indipendente di centro-sinistra) dal 21-6-2016'
-    const expected = {
-      nome: 'Giuseppe Sala',
-      partito: 'indipendente di centro-sinistra',
-      inizioCarica: '21/06/2016'
-    }
-    assert.deepEqual(utils.buildSindaco(tdText), expected)
-    done()
-  })
+  describe('buildSindaco should build the correct sindaco object in the case', function () {
+    it('of one mandate', function (done) {
+      const tdText = 'Giuseppe Sala (indipendente di centro-sinistra) dal 21-6-2016'
+      const expected = {
+        nome: 'Giuseppe Sala',
+        partito: 'indipendente di centro-sinistra',
+        inizioCarica: '21/06/2016'
+      }
+      assert.deepEqual(utils.buildSindaco(tdText), expected)
+      done()
+    })
 
-  it('buildSindaco should build the correct sindaco object in the case of two mandates', function (done) {
-    const tdText = "Nicola Ottaviani (Lega) dal 21-5-2012 (2º mandato dall'11-6-2017)"
-    const expected = {
-      nome: 'Nicola Ottaviani',
-      partito: 'Lega',
-      inizioCarica: '21/05/2012'
-    }
-    assert.deepEqual(utils.buildSindaco(tdText), expected)
-    done()
-  })
+    it('of two mandates', function (done) {
+      const tdText = "Nicola Ottaviani (Lega) dal 21-5-2012 (2º mandato dall'11-6-2017)"
+      const expected = {
+        nome: 'Nicola Ottaviani',
+        partito: 'Lega',
+        inizioCarica: '21/05/2012'
+      }
+      assert.deepEqual(utils.buildSindaco(tdText), expected)
+      done()
+    })
 
-  it('buildSindaco should build the correct sindaco object in the case of commissario prefettizio', function (done) {
-    const tdText = 'Commissario prefettizio dal 27-06-2018'
-    const expected = {
-      nome: 'Commissario prefettizio',
-      partito: '',
-      inizioCarica: '27/06/2018'
-    }
-    assert.deepEqual(utils.buildSindaco(tdText), expected)
-    done()
-  })
+    it('of commissario prefettizio', function (done) {
+      const tdText = 'Commissario prefettizio dal 27-06-2018'
+      const expected = {
+        nome: 'Commissario prefettizio',
+        partito: '',
+        inizioCarica: '27/06/2018'
+      }
+      assert.deepEqual(utils.buildSindaco(tdText), expected)
+      done()
+    })
 
-  it('buildSindaco should build the correct sindaco object in the case of date in DD/MM/YYYY format', function (done) {
-    const tdText = 'Andrea Vicini (Lista civica) dal 27/05/2019'
-    const expected = {
-      nome: 'Andrea Vicini',
-      partito: 'Lista civica',
-      inizioCarica: '27/05/2019'
-    }
-    assert.deepEqual(utils.buildSindaco(tdText), expected)
-    done()
-  })
+    it('of date in DD/MM/YYYY format', function (done) {
+      const tdText = 'Andrea Vicini (Lista civica) dal 27/05/2019'
+      const expected = {
+        nome: 'Andrea Vicini',
+        partito: 'Lista civica',
+        inizioCarica: '27/05/2019'
+      }
+      assert.deepEqual(utils.buildSindaco(tdText), expected)
+      done()
+    })
 
-  it('buildSindaco should build the correct sindaco object in the case of date with only the year', function (done) {
-    const tdText = 'Granata Ginetta (lista civica) dal 2016'
-    const expected = {
-      nome: 'Granata Ginetta',
-      partito: 'lista civica',
-      inizioCarica: '01/01/2016'
-    }
-    assert.deepEqual(utils.buildSindaco(tdText), expected)
-    done()
-  })
+    it('of date with only the year', function (done) {
+      const tdText = 'Granata Ginetta (lista civica) dal 2016'
+      const expected = {
+        nome: 'Granata Ginetta',
+        partito: 'lista civica',
+        inizioCarica: '01/01/2016'
+      }
+      assert.deepEqual(utils.buildSindaco(tdText), expected)
+      done()
+    })
 
-  it('buildSindaco should build the correct sindaco object in the case of date starting with dall\'', function (done) {
-    const tdText = 'Giovanni Enrico Caranzano (Lista civica "la Rinascita di Acceglio") dall\'11-6-2018'
-    const expected = {
-      nome: 'Giovanni Enrico Caranzano',
-      partito: 'Lista civica "la Rinascita di Acceglio"',
-      inizioCarica: '11/06/2018'
-    }
-    assert.deepEqual(utils.buildSindaco(tdText), expected)
-    done()
-  })
+    it('of date starting with dall\'', function (done) {
+      const tdText = 'Giovanni Enrico Caranzano (Lista civica "la Rinascita di Acceglio") dall\'11-6-2018'
+      const expected = {
+        nome: 'Giovanni Enrico Caranzano',
+        partito: 'Lista civica "la Rinascita di Acceglio"',
+        inizioCarica: '11/06/2018'
+      }
+      assert.deepEqual(utils.buildSindaco(tdText), expected)
+      done()
+    })
 
-  it('buildSindaco should build the correct sindaco object in the case of date with only one digit for the day', function (done) {
-    const tdText = 'Josi Gerardo Della Ragione (FreeBacoli) dal 9-6-2019'
-    const expected = {
-      nome: 'Josi Gerardo Della Ragione',
-      partito: 'FreeBacoli',
-      inizioCarica: '09/06/2019'
-    }
-    assert.deepEqual(utils.buildSindaco(tdText), expected)
-    done()
-  })
+    it('of date with only one digit for the day', function (done) {
+      const tdText = 'Josi Gerardo Della Ragione (FreeBacoli) dal 9-6-2019'
+      const expected = {
+        nome: 'Josi Gerardo Della Ragione',
+        partito: 'FreeBacoli',
+        inizioCarica: '09/06/2019'
+      }
+      assert.deepEqual(utils.buildSindaco(tdText), expected)
+      done()
+    })
 
-  it('buildSindaco should build the correct sindaco object in the case there is only the nome', function (done) {
-    const tdText = 'Renato Rizzo'
-    const expected = {
-      nome: 'Renato Rizzo',
-      partito: '',
-      inizioCarica: ''
-    }
-    assert.deepEqual(utils.buildSindaco(tdText), expected)
-    done()
+    it('there is only the nome', function (done) {
+      const tdText = 'Renato Rizzo'
+      const expected = {
+        nome: 'Renato Rizzo',
+        partito: '',
+        inizioCarica: ''
+      }
+      assert.deepEqual(utils.buildSindaco(tdText), expected)
+      done()
+    })
   })
 })
