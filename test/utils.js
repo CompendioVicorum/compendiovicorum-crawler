6edd054802ecf7316b509a9d33c1d2a6e07757c9
Davide Pastore <d.pastore@logispin.com>
2020-05-25 13:45:33 +0200

    Improve test structures and add two new cases

--- test/utils.js
+++ test/utils.js
@@ -32,26 +32,138 @@ describe('Utils', function () {
   })
 
   describe('buildSindaco should build the correct sindaco object in the case', function () {
-    it('of one mandate', function (done) {
-      const tdText = 'Giuseppe Sala (indipendente di centro-sinistra) dal 21-6-2016'
-      const expected = {
-        nome: 'Giuseppe Sala',
-        partito: 'indipendente di centro-sinistra',
-        inizioCarica: '21/06/2016'
-      }
-      assert.deepEqual(utils.buildSindaco(tdText), expected)
-      done()
-    })
+    describe('of date', function () {
+      it('with one mandate', function (done) {
+        const tdText = 'Giuseppe Sala (indipendente di centro-sinistra) dal 21-6-2016'
+        const expected = {
+          nome: 'Giuseppe Sala',
+          partito: 'indipendente di centro-sinistra',
+          inizioCarica: '21/06/2016'
+        }
+        assert.deepEqual(utils.buildSindaco(tdText), expected)
+        done()
+      })
 
-    it('of two mandates', function (done) {
-      const tdText = "Nicola Ottaviani (Lega) dal 21-5-2012 (2º mandato dall'11-6-2017)"
-      const expected = {
-        nome: 'Nicola Ottaviani',
-        partito: 'Lega',
-        inizioCarica: '21/05/2012'
-      }
-      assert.deepEqual(utils.buildSindaco(tdText), expected)
-      done()
+      it('with two mandates', function (done) {
+        const tdText = "Nicola Ottaviani (Lega) dal 21-5-2012 (2º mandato dall'11-6-2017)"
+        const expected = {
+          nome: 'Nicola Ottaviani',
+          partito: 'Lega',
+          inizioCarica: '21/05/2012'
+        }
+        assert.deepEqual(utils.buildSindaco(tdText), expected)
+        done()
+      })
+
+      it('with DD/MM/YYYY format', function (done) {
+        const tdText = 'Andrea Vicini (Lista civica) dal 27/05/2019'
+        const expected = {
+          nome: 'Andrea Vicini',
+          partito: 'Lista civica',
+          inizioCarica: '27/05/2019'
+        }
+        assert.deepEqual(utils.buildSindaco(tdText), expected)
+        done()
+      })
+
+      it('with only the year', function (done) {
+        const tdText = 'Granata Ginetta (lista civica) dal 2016'
+        const expected = {
+          nome: 'Granata Ginetta',
+          partito: 'lista civica',
+          inizioCarica: '01/01/2016'
+        }
+        assert.deepEqual(utils.buildSindaco(tdText), expected)
+        done()
+      })
+
+      it('starting with dall\'', function (done) {
+        const tdText = 'Giovanni Enrico Caranzano (Lista civica "la Rinascita di Acceglio") dall\'11-6-2018'
+        const expected = {
+          nome: 'Giovanni Enrico Caranzano',
+          partito: 'Lista civica "la Rinascita di Acceglio"',
+          inizioCarica: '11/06/2018'
+        }
+        assert.deepEqual(utils.buildSindaco(tdText), expected)
+        done()
+      })
+
+      it('with only one digit for the day', function (done) {
+        const tdText = 'Josi Gerardo Della Ragione (FreeBacoli) dal 9-6-2019'
+        const expected = {
+          nome: 'Josi Gerardo Della Ragione',
+          partito: 'FreeBacoli',
+          inizioCarica: '09/06/2019'
+        }
+        assert.deepEqual(utils.buildSindaco(tdText), expected)
+        done()
+      })
+
+      it('with the month name', function (done) {
+        const tdText = 'Gian Mario Morello (lista civica Balocco e Bastia Insieme) dal 10 giugno 2018 (2º mandato)'
+        const expected = {
+          nome: 'Gian Mario Morello',
+          partito: 'lista civica Balocco e Bastia Insieme',
+          inizioCarica: '10/06/2018'
+        }
+        assert.deepEqual(utils.buildSindaco(tdText), expected)
+        done()
+      })
+
+      it('with the month name without the day', function (done) {
+        const tdText = 'Maria Rosa Gnocchi (lista civica) dal giugno 2015'
+        const expected = {
+          nome: 'Maria Rosa Gnocchi',
+          partito: 'lista civica',
+          inizioCarica: '01/06/2015'
+        }
+        assert.deepEqual(utils.buildSindaco(tdText), expected)
+        done()
+      })
+
+      it('with the month and the year in the MM/YYYY format', function (done) {
+        const tdText = 'Marco Corti (lista civica dal 1999) dal 05/2019'
+        const expected = {
+          nome: 'Marco Corti',
+          partito: 'lista civica dal 1999',
+          inizioCarica: '01/05/2019'
+        }
+        assert.deepEqual(utils.buildSindaco(tdText), expected)
+        done()
+      })
+
+      it('with DD/MM/YYYY format with also "-"', function (done) {
+        const tdText = "Gianpaolo Beretta (lista civica di centrosinistra Impegno per Borgo) dall'08/05/2012 - 2º mandato"
+        const expected = {
+          nome: 'Gianpaolo Beretta',
+          partito: 'lista civica di centrosinistra Impegno per Borgo',
+          inizioCarica: '08/05/2012'
+        }
+        assert.deepEqual(utils.buildSindaco(tdText), expected)
+        done()
+      })
+
+      it('with DD/MM/YY format', function (done) {
+        const tdText = 'Roberto Valettini (lista civica Aulla nel cuore) dal 12-6-17'
+        const expected = {
+          nome: 'Roberto Valettini',
+          partito: 'lista civica Aulla nel cuore',
+          inizioCarica: '12/06/2017'
+        }
+        assert.deepEqual(utils.buildSindaco(tdText), expected)
+        done()
+      })
+
+      it('with DD.MM.YYYY format', function (done) {
+        const tdText = 'Paolo Maria Belluardo (Calliano nel 2000) dal 25.05.2014'
+        const expected = {
+          nome: 'Paolo Maria Belluardo',
+          partito: 'Calliano nel 2000',
+          inizioCarica: '25/05/2014'
+        }
+        assert.deepEqual(utils.buildSindaco(tdText), expected)
+        done()
+      })
     })
 
     it('of commissario prefettizio', function (done) {
@@ -65,111 +177,23 @@ describe('Utils', function () {
       done()
     })
 
-    it('of date in DD/MM/YYYY format', function (done) {
-      const tdText = 'Andrea Vicini (Lista civica) dal 27/05/2019'
-      const expected = {
-        nome: 'Andrea Vicini',
-        partito: 'Lista civica',
-        inizioCarica: '27/05/2019'
-      }
-      assert.deepEqual(utils.buildSindaco(tdText), expected)
-      done()
-    })
-
-    it('of date with only the year', function (done) {
-      const tdText = 'Granata Ginetta (lista civica) dal 2016'
-      const expected = {
-        nome: 'Granata Ginetta',
-        partito: 'lista civica',
-        inizioCarica: '01/01/2016'
-      }
-      assert.deepEqual(utils.buildSindaco(tdText), expected)
-      done()
-    })
-
-    it('of date starting with dall\'', function (done) {
-      const tdText = 'Giovanni Enrico Caranzano (Lista civica "la Rinascita di Acceglio") dall\'11-6-2018'
-      const expected = {
-        nome: 'Giovanni Enrico Caranzano',
-        partito: 'Lista civica "la Rinascita di Acceglio"',
-        inizioCarica: '11/06/2018'
-      }
-      assert.deepEqual(utils.buildSindaco(tdText), expected)
-      done()
-    })
-
-    it('of date with only one digit for the day', function (done) {
-      const tdText = 'Josi Gerardo Della Ragione (FreeBacoli) dal 9-6-2019'
-      const expected = {
-        nome: 'Josi Gerardo Della Ragione',
-        partito: 'FreeBacoli',
-        inizioCarica: '09/06/2019'
-      }
-      assert.deepEqual(utils.buildSindaco(tdText), expected)
-      done()
-    })
-
-    it('of date with the month name', function (done) {
-      const tdText = 'Gian Mario Morello (lista civica Balocco e Bastia Insieme) dal 10 giugno 2018 (2º mandato)'
-      const expected = {
-        nome: 'Gian Mario Morello',
-        partito: 'lista civica Balocco e Bastia Insieme',
-        inizioCarica: '10/06/2018'
-      }
-      assert.deepEqual(utils.buildSindaco(tdText), expected)
-      done()
-    })
-
-    it('of date with the month name without the day', function (done) {
-      const tdText = 'Maria Rosa Gnocchi (lista civica) dal giugno 2015'
-      const expected = {
-        nome: 'Maria Rosa Gnocchi',
-        partito: 'lista civica',
-        inizioCarica: '01/06/2015'
-      }
-      assert.deepEqual(utils.buildSindaco(tdText), expected)
-      done()
-    })
-
-    it('of date with the month and the year in the MM/YYYY format', function (done) {
-      const tdText = 'Marco Corti (lista civica dal 1999) dal 05/2019'
-      const expected = {
-        nome: 'Marco Corti',
-        partito: 'lista civica dal 1999',
-        inizioCarica: '01/05/2019'
-      }
-      assert.deepEqual(utils.buildSindaco(tdText), expected)
-      done()
-    })
-
-    it('of date in DD/MM/YYYY format with also "-"', function (done) {
-      const tdText = "Gianpaolo Beretta (lista civica di centrosinistra Impegno per Borgo) dall'08/05/2012 - 2º mandato"
-      const expected = {
-        nome: 'Gianpaolo Beretta',
-        partito: 'lista civica di centrosinistra Impegno per Borgo',
-        inizioCarica: '08/05/2012'
-      }
-      assert.deepEqual(utils.buildSindaco(tdText), expected)
-      done()
-    })
-
-    it('of date in DD/MM/YY format', function (done) {
-      const tdText = 'Roberto Valettini (lista civica Aulla nel cuore) dal 12-6-17'
-      const expected = {
-        nome: 'Roberto Valettini',
-        partito: 'lista civica Aulla nel cuore',
-        inizioCarica: '12/06/2017'
-      }
-      assert.deepEqual(utils.buildSindaco(tdText), expected)
-      done()
-    })
-
     it('there is only the nome', function (done) {
       const tdText = 'Renato Rizzo'
       const expected = {
         nome: 'Renato Rizzo',
         partito: '',
         inizioCarica: ''
+      }
+      assert.deepEqual(utils.buildSindaco(tdText), expected)
+      done()
+    })
+
+    it('there are parentheses in the partito field', function (done) {
+      const tdText = 'Paolo Maria Belluardo (Calliano nel 2000 (lista civica)) dal 2014'
+      const expected = {
+        nome: 'Paolo Maria Belluardo',
+        partito: 'Calliano nel 2000 (lista civica)',
+        inizioCarica: '01/01/2014'
       }
       assert.deepEqual(utils.buildSindaco(tdText), expected)
       done()
